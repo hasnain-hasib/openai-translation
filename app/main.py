1bce9efaf26a6a7f898e72ff9a0f8f22b59c1499
Hasnain Hasib <106244222+hasnain-hasib@users.noreply.github.com>
2024-09-02 23:02:32 +0600

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,9 +1,10 @@
-from fastapi import FastAPI, BackgroundTasks,Request,Depends
+from fastapi import FastAPI, BackgroundTasks,Request,Depends, HTTPException
 from sqlalchemy.orm import Session
 from fastapi.responses import HTMLResponse
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.templating import Jinja2Templates
 from app import schemas, crud, model
+from app.utils import perform_translation
 from app.database import engine, get_db
 
 model.Base.metadata.create_all(bind= engine)
@@ -30,11 +31,27 @@ def index(request: Request):
 
 
 
-@app.post('/translet',response_model=schemas.TaskResponse)
+@app.post('/translate',response_model=schemas.TaskResponse)
 def translate (request: schemas.TranslationRequest, background_tasks :BackgroundTasks, db : Session  = Depends(get_db)):
-     task = crud.create_Translation_Task(get_db, request.text, request.languages)
+     task = crud.create_Translation_Task(db, request.text, request.languages)
 
      background_tasks.add_task(perform_translation, task.id, request.text, request.languages, db)
-     
-     
-     return
+     return {"task_id": task.id}
+ 
+ 
+@app.get("/translate/{task_id}",response_model=schemas.TranslationStatus)
+def get_translate(task_id : int , db : Session= Depends (get_db)):
+    task = crud.get_translation_task(db , task_id)
+    if not task :
+        raise HTTPException(status_code = 404, details ="task not found")
+    return {"task_id": task.id  ,"status" : task.status, "translation" : task.translation}
+ 
+ 
+@app.get("/translate/content/{task_id}",response_model=schemas.TranslationStatus)
+def get_translate(task_id : int , db : Session= Depends (get_db)):
+    task = crud.get_translation_task(db , task_id)
+    if not task :
+        raise HTTPException(status_code = 404, details ="task not found")
+    return {task}
+
+
